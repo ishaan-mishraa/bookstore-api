f08c8c3e9a65ed88e1586603be55672b70a3c918
Ishaan Mishra <ishaancodes01@gmail.com>
2025-04-09 10:16:48 +0530

    changed database config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,7 +5,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'Ishaan@123',
+    password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'bookstore',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: true,
